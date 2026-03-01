a8bbdfe5cb6c33214a0656a86cb42dca72128fa4
verticalshifts01 <faisal.imgs@gmail.com>
2026-03-02 01:00:28 +0600

    Update main.tsx
    added following
    import { ApplicationInsights } from '@microsoft/applicationinsights-web'
    
    const appInsights = new ApplicationInsights({ config: {
      connectionString: 'InstrumentationKey=72b9d2ff-c31c-4828-b32b-424d88cd9c80;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=e3c50dd7-a367-4c9f-bfe6-1e64a807b480' 
      /* Tip: Use an environment variable here like import.meta.env.VITE_APPINSIGHTS_STRING */
    }});
    appInsights.loadAppInsights();
    appInsights.trackPageView();

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+
+const appInsights = new ApplicationInsights({ config: {
+  connectionString: 'InstrumentationKey=72b9d2ff-c31c-4828-b32b-424d88cd9c80;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=e3c50dd7-a367-4c9f-bfe6-1e64a807b480' 
+  /* Tip: Use an environment variable here like import.meta.env.VITE_APPINSIGHTS_STRING */
+}});
+appInsights.loadAppInsights();
+appInsights.trackPageView();
+
 createRoot(document.getElementById("root")!).render(<App />);
