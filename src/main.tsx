42326047a95911360ffe1be1c5d87f7ba6dd6cc0
verticalshifts01 <faisal.imgs@gmail.com>
2026-03-02 06:09:51 +0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,19 @@ import "./index.css";
 
 import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
-const appInsights = new ApplicationInsights({ config: {
-  connectionString: 'InstrumentationKey=72b9d2ff-c31c-4828-b32b-424d88cd9c80;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=e3c50dd7-a367-4c9f-bfe6-1e64a807b480' 
-  /* Tip: Use an environment variable here like import.meta.env.VITE_APPINSIGHTS_STRINGS */
-}});
+cconsole.log("DEBUG: App Insights Init Starting...");
+
+const appInsights = new ApplicationInsights({ 
+  config: {
+    connectionString: 'InstrumentationKey=72b9d2ff-c31c-4828-b32b-424d88cd9c80;IngestionEndpoint=https://southeastasia-1.in.applicationinsights.azure.com/;LiveEndpoint=https://southeastasia.livediagnostics.monitor.azure.com/;ApplicationId=e3c50dd7-a367-4c9f-bfe6-1e64a807b480',
+    loggingLevelConsole: 2, // Forces SDK to log to console
+    enableDebug: true       // Enables internal SDK debugging
+  } 
+});
+
 appInsights.loadAppInsights();
 appInsights.trackPageView();
+
+console.log("DEBUG: App Insights Init Finished!");
 
 createRoot(document.getElementById("root")!).render(<App />);
